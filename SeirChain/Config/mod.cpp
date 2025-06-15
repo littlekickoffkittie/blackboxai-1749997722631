8b337304856d68ef896c277774c01b5e349d2c76
littlekickoffkittie <financingcanfunk@gmail.com>
2025-06-15 10:28:50 -0400

    Add SeirChain/Config/mod.cpp 1749997730614

---
+++ SeirChain/Config/mod.cpp
@@ -0,0 +1,10 @@
+// mod.cpp
+// Module initialization and configuration aggregation for SeirChain
+
+#include "Config.hpp"
+#include <memory>
+
+namespace SeirChain {
+    // This file can be used to initialize modules or aggregate configurations if needed.
+    // Currently, configuration loading is handled in Config.cpp.
+}
